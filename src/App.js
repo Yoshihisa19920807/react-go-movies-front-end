b64e300362cfd995dd4a7ac7d4f3c16000fa4f46
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-30 11:12:10 +0900

    mod App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 function App() {
-  return <div>Go watch a movie</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go watch a movie</h1>
+        </div>
+        <div className="col text-end">
+          <a href="">
+            <span className="badge bg-success">Login</span>
+          </a>
+        </div>
+        <hr className="mb-3"></hr>
+      </div>
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#!" className="list-group-item list-group-item-action">
+                Home
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Movies
+              </a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
