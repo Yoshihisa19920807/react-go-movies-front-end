16ec819bd623027ceb45a59d1658b648f1447674
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-30 11:28:27 +0900

    implement react router to index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([{ path: '/', element: <App /> }]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
