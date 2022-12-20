fa42c9efdd925fc0247aa6696d0901344af05078
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-12-20 17:23:31 +0900

    login with db data

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, useOutletContext } from 'react-router-dom';
-import Input from './form/input';
+import Input from './form/Input';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -13,24 +13,57 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('email/pass', email, password);
-    if (email === 'admin@example.com') {
-      setJwtToken('abc');
-      setAlertClassName('alert-success');
-      setAlertMessage('Login Succcess');
-      setTimeout(() => {
-        setAlertClassName('d-none');
-        setAlertMessage('');
-      }, 5000);
-      navigate('/');
-    } else {
-      setAlertClassName('alert-danger');
-      setAlertMessage('Invalid Credential');
-      setTimeout(() => {
-        setAlertClassName('d-none');
-        setAlertMessage('');
-      }, 5000);
-    }
+    // console.log('email/pass', email, password);
+    // if (email === 'admin@example.com') {
+    //   setJwtToken('abc');
+    //   setAlertClassName('alert-success');
+    //   setAlertMessage('Login Succcess');
+    //   setTimeout(() => {
+    //     setAlertClassName('d-none');
+    //     setAlertMessage('');
+    //   }, 5000);
+    //   navigate('/');
+    // } else {
+    //   setAlertClassName('alert-danger');
+    //   setAlertMessage('Invalid Credential');
+    //   setTimeout(() => {
+    //     setAlertClassName('d-none');
+    //     setAlertMessage('');
+    //   }, 5000);
+    // }
+
+    // build the request payload
+    let payload = {
+      email: email,
+      password: password,
+    };
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(payload),
+    };
+
+    fetch(`/authenticate`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          setAlertClassName('alert-danger');
+          setAlertMessage(data.message);
+        } else {
+          setJwtToken(data.access_token);
+          setAlertClassName('d-none');
+          setAlertMessage('');
+          navigate('/');
+        }
+      })
+      .catch((error) => {
+        setAlertClassName('alert-danger');
+        setAlertMessage(error);
+      });
   };
   return (
     <>
