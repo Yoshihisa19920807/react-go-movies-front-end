edcf01f43285dfb91d86d7c16b6be36ba56c1669
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-12-22 09:50:33 +0900

    create TextArea component

---
+++ src/components/form/TextArea.js
@@ -0,0 +1,21 @@
+const TextArea = (props) => {
+  return (
+    <div className="mb-3">
+      <label htmlFor={props.name} className="form-label">
+        {props.title}
+      </label>
+      <textarea
+        className="form-control"
+        id={props.name}
+        name={props.name}
+        value={props.value}
+        onChange={props.onChange}
+        rows={props.row}
+      ></textarea>
+
+      <div className={props.errorDiv}>{props.errorMsg}</div>
+    </div>
+  );
+};
+
+export default TextArea;
