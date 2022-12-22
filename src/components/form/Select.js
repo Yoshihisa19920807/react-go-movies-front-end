416f82ab304fdb4badb464ed75092d7e249d0c78
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-12-22 09:50:14 +0900

    create Select component

---
+++ src/components/form/Select.js
@@ -0,0 +1,22 @@
+const Select = (props) => {
+  return (
+    <div className="mb-3">
+      <label htmlFor={props.name} className="form-label"></label>
+      <select
+        className="form-select"
+        name={props.name}
+        id={props.name}
+        value={props.value}
+        onChange={props.onChange}
+      >
+        <option value="">{props.placeHolder}</option>
+        {props.options.map((option) => {
+          return <option key={option.id} value={option.value}></option>;
+        })}
+      </select>
+      <div className={props.errorDiv}>{props.errorMsg}</div>
+    </div>
+  );
+};
+
+export default Select;
